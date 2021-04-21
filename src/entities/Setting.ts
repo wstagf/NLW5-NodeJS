314ac20403a341cf7406deb9e6ba4000c2dfb629
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-20 23:24:14 -0300

    adicionado anotacoes à entidade de settings

--- src/entities/Setting.ts
+++ src/entities/Setting.ts
@@ -1,9 +1,21 @@
+import { Entity, Column, CreateDateColumn, UpdateDateColumn, PrimaryColumn } from "typeorm";
 
+@Entity("settings")
 class Setting {
+
+    @PrimaryColumn()
     id: string;
+
+    @Column()
     username: string;
+
+    @Column()
     chat: boolean;
+
+    @UpdateDateColumn()
     update_at: Date;
+
+    @CreateDateColumn()
     created_at: Date;
 }
 
