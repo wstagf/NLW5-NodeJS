f60eadff934444ebafed5a34af1a3523eca6355a
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2021-04-20 23:38:35 -0300

    Criado primeiro repositorio

---
+++ src/repositories/SettingsRepository.ts
@@ -0,0 +1,11 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Setting } from "../entities/Setting";
+
+@EntityRepository(Setting)
+class SettingsRepository extends Repository<Setting> {
+
+
+    
+}
+
+export { SettingsRepository }
